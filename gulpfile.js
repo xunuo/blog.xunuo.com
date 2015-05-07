3006be3f249bce73bdded698f1c339b30f4e8038
nuo.xun <nuo.xun@alibaba-inc.com>
2015-05-07 15:47:35 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp'),
-    livereload = require('gulp-livereload'),
     shell = require('gulp-shell'),
     serverPort = 1234,
     enConfigString = ' --config _config-en.yml',
@@ -11,47 +10,29 @@ var gulp = require('gulp'),
  */
 gulp.task('add-new-blog', shell.task([
   'hexo new this-is-a-new-blog'
-]))
+]));
 
 /**
  * 新增页面 (to source root base)
  */
 gulp.task('add-new-page', shell.task([
   'hexo new page this-is-a-new-page'
-]))
+]));
 
 /**
  * 运行服务
  */
 gulp.task('server', shell.task([
   'hexo server -p ' + serverPort
-]))
+]));
 
 /**
- * 内容或风格变动自动刷新
+ * 终止残余服务
  */
-//gulp.task('watch_source_themes', function() {
-//  livereload.listen();
-//  gulp.watch(['source/**','themes/**'], function(event){  
-//      console.log('[!] File changed:\n');
-//      setTimeout(function(){
-//          var consoleInfo = event.path.replace(__dirname,'');
-//          livereload.changed(consoleInfo);
-//      },1000);
-//  });  
-//});
+gulp.task('kill-server', shell.task([
+  'kill -9 $(lsof -i:' + serverPort + ' |awk \'{print $2}\' | tail -n 2)'
+]));
 
-/**
- * 内容或风格变动自动刷新
- */
-//gulp.task('watch_config', function() {
-//  livereload.listen();
-//  gulp.watch(['_config.yml','*/_config.yml'], function(event){
-//      console.log('[!] Config changed:\n');
-//      var consoleInfo = event.path.replace(__dirname,'');
-//      livereload.changed(consoleInfo);
-//  });
-//});
 
 
 /**
@@ -63,23 +44,10 @@ gulp.task('clean', shell.task([
   'rm -rf .deploy_git'
 ]));
 
-/**
- * 终止残余服务
- */
-gulp.task('kill-server', shell.task([
-  'kill -9 $(lsof -i:' + serverPort + ' |awk \'{print $2}\' | tail -n 2)'
-]));
-
 
 /**
  * 发布上线
  */
-//gulp.task('publish-all',['publish-to-cn'],shell.task([
-//  'echo "正在推送到 Github Pages ..."',
-//  'hexo d -g' + enConfigString,
-//  'echo "已推送到 Github Pages。"'
-//]))
-
 // 'PP' == 'push-and-publish'
 gulp.task('PP',shell.task([
   
@@ -96,18 +64,5 @@ gulp.task('PP',shell.task([
   'echo "成功推送到 Gitcafe Pages。"'
     
 ]))
-
-
-/**
- * 同步到Gitcafe
- */
-//gulp.task('publish-to-cn', shell.task([
-//  'echo "正在推送到 Gitcafe Pages ..."',
-//  'hexo d -g' + cnConfigString,
-//  'echo "已推送到 Gitcafe Pages。"'
-//]))
-
-
-
 
 gulp.task('default', ['server','watch_source_themes','watch_config']);
